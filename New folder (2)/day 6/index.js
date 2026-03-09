ed5de87b8d380b98e1600009ed9e4f09e6c17ad8
Piyush Kumar <piyushpanwarrr@gmail.com>
2026-03-09 13:15:09 +0530

    Add files via upload

---
+++ New folder (2)/day 6/index.js
@@ -0,0 +1,6 @@
+const data=9;
+    const heading= document.getElementById("headingid");
+    
+    headingid.style.color="yellow";
+    const button = document.getElementById("buttonid");
+    button.disabled =true;
